6fef127409326ce684c984ff9775659de1836503
JustContestIt <fantomasqwe17@gmail.com>
2022-05-13 11:03:27 +0300

    Finished guide for Exercise #10

--- Exercise#10/main.py
+++ Exercise#10/main.py
@@ -21,6 +21,28 @@ if __name__ == "__main__":
     imagePaths = sorted(list(paths.list_images('train')))
     trainData = []
     labels = []
+    c = 0.0
+    rs = 0
+    s = ''
+    s1 = ''
+    criterion = ''
+    msl = 0
+    mln = 0
+    n = 0
+    cv = 0
+    solver = ''
+
+    print("\nСкачайте архивы и разархивируйте их содержимое в папки 'train' и 'test' соответственно. "
+          "Названия папок должны соответствовать этим названиям, иначе меняйте код:)")
+    c = float(input("Введите значение С -> "))
+    s = input("Введите параметры для бэггинга в таком виде 'criterion min_samples_leaf max_leaf_nodes "
+              "random_state n_estimators'\n Пример: entropy 10 20 462 19 -> ")
+    solver = input("Введите значение solver -> ")
+    cv = int(input("Введите значение CV -> "))
+    s1 = input("Введите список названий картинок в таком виде 'dog.1001 dog.1002 cat.1003 cat.1004' -> ")
+    print("\nПодожди немного\n")
+    s1 = s1.split()
+    s = s.split()
 
     for (i, imagePath) in enumerate(imagePaths):
         image = cv2.imread(imagePath, 1)
@@ -32,57 +54,56 @@ if __name__ == "__main__":
     Y = [1 if x == 'cat' else 0 for x in labels]
 
     # ------------------------------------------------------------------------
-    tree = DecisionTreeClassifier(criterion='entropy',  # критерий разделения
-                                  min_samples_leaf=10,  # минимальное число объектов в листе
-                                  max_leaf_nodes=20,  # максимальное число листьев
-                                  random_state=462)
+    tree = DecisionTreeClassifier(criterion=s[0],  # критерий разделения
+                                  min_samples_leaf=int(s[1]),  # минимальное число объектов в листе
+                                  max_leaf_nodes=int(s[2]),  # максимальное число листьев
+                                  random_state=int(s[3]))
     bagging = BaggingClassifier(tree,  # базовый алгоритм
-                                n_estimators=19,  # количество деревьев
-                                random_state=462)
+                                n_estimators=int(s[4]),  # количество деревьев
+                                random_state=int(s[3]))
     bagging.fit(trainData, Y)
     # ------------------------------------------------------------------------
-    svm = LinearSVC(random_state=462, C=1.09)
+    svm = LinearSVC(random_state=rs, C=c)
     svm.fit(trainData, Y)
     # ------------------------------------------------------------------------
-    forest = RandomForestClassifier(n_estimators=19,  # количество деревьев
-                                    criterion='entropy',  # критерий разделения
-                                    min_samples_leaf=10,  # минимальное число объектов в листе
-                                    max_leaf_nodes=20,  # максимальное число листьев
-                                    random_state=462)
+    forest = RandomForestClassifier(n_estimators=int(s[4]),  # количество деревьев
+                                    criterion=s[0],  # критерий разделения
+                                    min_samples_leaf=int(s[1]),  # минимальное число объектов в листе
+                                    max_leaf_nodes=int(s[2]),  # максимальное число листьев
+                                    random_state=int(s[3]))
     forest.fit(trainData, Y)
     # ------------------------------------------------------------------------
-    lr = LogisticRegression(solver='lbfgs', random_state=462)
+    lr = LogisticRegression(solver=solver, random_state=int(s[3]))
     # ------------------------------------------------------------------------
     base_estimators = [('SVM', svm), ('Bagging DT', bagging), ('DecisionForest', forest)]
-    sclf = StackingClassifier(estimators=base_estimators, final_estimator=lr, cv=2)  # здесь CV, изменить
+    sclf = StackingClassifier(estimators=base_estimators, final_estimator=lr, cv=cv)  # здесь CV, изменить
     sclf.fit(trainData, Y)
     # ------------------------------------------------------------------------
-    singleImage = cv2.imread('test/dog.1023.jpg')  # вставлять сюда
+    singleImage = cv2.imread('test/' + s1[0] + '.jpg')  # вставлять сюда
     histt_1 = extract_histogram(singleImage)
     histt1 = histt_1.reshape(1, -1)
     prediction1 = sclf.predict(histt1)
     # ------------------------------------------------------------------------
-    singleImage = cv2.imread('test/dog.1029.jpg')  # вставлять сюда
+    singleImage = cv2.imread('test/' + s1[1] + '.jpg')  # вставлять сюда
     histt_2 = extract_histogram(singleImage)
     histt2 = histt_2.reshape(1, -1)
     prediction2 = sclf.predict(histt2)
     # ------------------------------------------------------------------------
-    singleImage = cv2.imread('test/dog.1006.jpg')  # вставлять сюда
+    singleImage = cv2.imread('test/' + s1[2] + '.jpg')  # вставлять сюда
     histt_3 = extract_histogram(singleImage)
     histt3 = histt_3.reshape(1, -1)
     prediction3 = sclf.predict(histt3)
     # ------------------------------------------------------------------------
-    singleImage = cv2.imread('test/dog.1004.jpg')  # вставлять сюда
+    singleImage = cv2.imread('test/' + s1[3] + '.jpg')  # вставлять сюда
     histt_4 = extract_histogram(singleImage)
     histt4 = histt_4.reshape(1, -1)
     prediction4 = sclf.predict(histt4)
     # ------------------------------------------------------------------------
-    print(sclf.score(trainData, Y))
-    print(prediction1)
-    print(sclf.predict_proba(histt1))
-    print(prediction2)
-    print(sclf.predict_proba(histt2))
-    print(prediction3)
-    print(sclf.predict_proba(histt3))
-    print(prediction4)
-    print(sclf.predict_proba(histt4))
+
+    print("Ответы\n------------------------------------------------------------------------")
+    print("Первое задание: " + str(sclf.score(trainData, Y)) + "\n")
+    print("Второе задание:")
+    print("Первая картинка: " + str(round(float(sclf.predict_proba(histt1)[0][1]), 3)))
+    print("Вторая картинка: " + str(round(float(sclf.predict_proba(histt2)[0][1]), 3)))
+    print("Третья картинка: " + str(round(float(sclf.predict_proba(histt3)[0][1]), 3)))
+    print("Четвертая картинка: " + str(round(float(sclf.predict_proba(histt4)[0][1]), 3)))
